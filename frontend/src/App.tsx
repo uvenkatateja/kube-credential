d89bf72c63700983ba18deb35d0a14fd4c89169f
uvenkatateja <uvteja1111@gmail.com>
2025-10-07 00:37:11 +0530

    fix frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom';
 import Issue from './pages/Issue';
 import Verify from './pages/Verify';
